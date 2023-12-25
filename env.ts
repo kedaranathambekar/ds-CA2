c4a2c55338981cc108b29d92c8ce595a170580cc
kedaranthambekar <20091631@mail.wit.ie>
2023-12-25 21:31:31 +0000

    initial commit

--- env.ts
+++ env.ts
@@ -1,3 +1,3 @@
 export const SES_REGION = 'eu-west-1';
 export const SES_EMAIL_FROM = 'kedhar396732@gmail.com' ;
-export const SES_EMAIL_TO =  'kedhar3967@gmail.com';
+export const SES_EMAIL_TO =  'kedhar396732@gmail.com';
