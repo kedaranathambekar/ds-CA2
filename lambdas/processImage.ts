eaf0e2dde1bc4df8452819ea453f1565d50feec9
Kedaranath Ambekar <72230613+kedaranathambekar@users.noreply.github.com>
2024-01-01 17:58:28 +0000

    Add processimage

---
+++ lambdas/processImage.ts
@@ -0,0 +1,66 @@
+/* eslint-disable import/extensions, import/no-absolute-path */
+import { SQSHandler } from "aws-lambda";
+// import { sharp } from "/opt/nodejs/sharp-utils";
+import {
+  GetObjectCommand,
+  PutObjectCommandInput,
+  GetObjectCommandInput,
+  S3Client,
+  PutObjectCommand,
+} from "@aws-sdk/client-s3";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, PutCommand } from "@aws-sdk/lib-dynamodb";
+
+const ddbDocClient = createDDbDocClient();
+const s3 = new S3Client();
+
+export const handler: SQSHandler = async (event) => {
+  console.log("Event ", event);
+  for (const record of event.Records) {
+    const recordMessage = JSON.parse(JSON.parse(record.body).Message);
+    console.log('Raw SNS message ', JSON.stringify(recordMessage))
+    if (recordMessage.Records) {
+      for (const messageRecord of recordMessage.Records) {
+        const s3e = messageRecord.s3;
+        const srcBucket = s3e.bucket.name;
+        // Object key may have spaces or unicode non-ASCII characters.
+        const srcKey = decodeURIComponent(s3e.object.key.replace(/\+/g, " "));
+        // Infer the image type from the file suffix.
+        const typeMatch = srcKey.match(/\.([^.]*)$/);
+        if (!typeMatch) {
+          console.log("Could not determine the image type.");
+          throw new Error("Could not determine the image type. ");
+        }
+        // Check that the image type is supported
+        const imageType = typeMatch[1].toLowerCase();
+        if (imageType != "jpeg" && imageType != "png") {
+          console.log(`Unsupported image type: ${imageType}`);
+          throw new Error("Unsupported image type: ${imageType. ");
+        }
+        
+        const putCommand = new PutCommand({
+          TableName: "Images",
+          Item: {
+            ImageName: srcKey,
+          },
+        });
+
+        await ddbDocClient.send(putCommand);
+      }
+    }
+  }
+};
+
+function createDDbDocClient() {
+  const ddbClient = new DynamoDBClient({ region: process.env.REGION });
+  const marshallOptions = {
+    convertEmptyValues: true,
+    removeUndefinedValues: true,
+    convertClassInstanceToMap: true,
+  };
+  const unmarshallOptions = {
+    wrapNumbers: false,
+  };
+  const translateConfig = { marshallOptions, unmarshallOptions };
+  return DynamoDBDocumentClient.from(ddbClient, translateConfig);
+}
